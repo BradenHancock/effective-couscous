08e4f9c3b72fbd9191b20827930a074265d1af74
Braden Hancock <>
2022-04-08 14:14:30 +0100

    Test

--- Package.swift
+++ Package.swift
@@ -1,31 +1,20 @@
 // swift-tools-version:5.3
+
 import PackageDescription
 
 let package = Package(
     name: "iProov",
     products: [
-        .library(
-            name: "iProov",
-            targets: ["iProov"]
-        ),
-        .library(
-            name: "SocketIO",
-            type: .dynamic,
-            targets: ["SocketIO"]
-        ),
+        .library(name: "iProov", targets: ["iProov"]),
     ],
     dependencies: [
         .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "16.0.1"))
     ],
     targets: [
-//        .target(
-//            name: "SocketIO",
-//            dependencies: ["Starscream"]
-//        ),
         .binaryTarget(
             name: "iProov",
             url: "https://github.com/BradenHancock/effective-couscous/raw/main/iProov.xcframework.zip",
             checksum: "a37807600b6fa386bf42388cc4f43c84f2fb67a84e884dd4ec5667d0dba33430"
-        ),
+        )
     ]
 )
