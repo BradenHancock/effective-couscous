a42b008b7032ee4906e67e1016a2b96244b35316
Braden Hancock <>
2022-04-08 17:37:17 +0100

    Delete class

--- SwiftPM/Dummy.swift
+++ SwiftPM/Dummy.swift
@@ -1,9 +1,1 @@
-public class Dummy {
-    public init() {
-        print("Dummy initialised")
-    }
-    
-    public func doSomething() {
-        print("Doing something not very useful!")
-    }
-}
+
