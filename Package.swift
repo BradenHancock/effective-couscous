be2be62a2dc5ed8ecc252e0639158ff0915c9941
Braden Hancock <>
2022-04-08 17:36:44 +0100

    Try SocketIO.xcframework

--- Package.swift
+++ Package.swift
@@ -6,11 +6,8 @@ let package = Package(
         .library(
             name: "iProov",
             type: .dynamic,
-            targets: ["iProovWrapper"]
+            targets: ["iProovTargets"]
         )
-    ],
-    dependencies: [
-        .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "16.0.1"))
     ],
     targets: [
         .binaryTarget(
@@ -18,11 +15,22 @@ let package = Package(
             url: "https://github.com/BradenHancock/effective-couscous/raw/main/iProov.xcframework.zip",
             checksum: "a37807600b6fa386bf42388cc4f43c84f2fb67a84e884dd4ec5667d0dba33430"
         ),
-        .target(name: "iProovWrapper",
+        .binaryTarget(
+            name: "SocketIO",
+            url: "https://github.com/BradenHancock/effective-couscous/raw/main/SocketIO.xcframework.zip",
+            checksum: "da0534a872cfc646c8e2d4269d53466796aaee412be0526e17a4ddfa4d8902ab"
+        ),
+        .target(name: "iProovTargets",
             dependencies: [
-                .product(name: "SocketIO", package: "socket.io-client-swift")
+                .target(name: "iProov", condition: .when(platforms: .some([.iOS]))),
+                .target(name: "SocketIO", condition: .when(platforms: .some([.iOS])))
             ],
-            path: "./SwiftPM"
+            path: "SwiftPM"
         )
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
+
+//dependencies: [
+//    .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "16.0.1"))
+//],
