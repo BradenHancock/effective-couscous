2afbf47c9bb51c5e5a4e8dff277873ceff125272
Braden Hancock <>
2022-04-11 14:13:27 +0100

    Add Starscream as binary target

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@
 
 // Trying to replicate this method https://forums.swift.org/t/swiftpm-binary-target-with-sub-dependencies/40197/7
 // using binary targets, and using SpmTestApp to integrate (but it has errors)
+
 import PackageDescription
 
 let package = Package(
@@ -26,10 +27,16 @@ let package = Package(
             url: "https://github.com/BradenHancock/effective-couscous/raw/main/SocketIO.xcframework.zip",
             checksum: "da0534a872cfc646c8e2d4269d53466796aaee412be0526e17a4ddfa4d8902ab"
         ),
+        .binaryTarget(
+            name: "Starscream",
+            url: "https://github.com/BradenHancock/effective-couscous/raw/main/Starscream.xcframework.zip",
+            checksum: "bfcab1c2aa13351029e2983bbe7a50ec30eb0fdc812ae611d681f6a4c2574de1"
+        ),
         .target(name: "iProovTargets",
             dependencies: [
                 .target(name: "iProov", condition: .when(platforms: .some([.iOS]))),
-                .target(name: "SocketIO", condition: .when(platforms: .some([.iOS])))
+                .target(name: "SocketIO", condition: .when(platforms: .some([.iOS]))),
+                .target(name: "Starscream", condition: .when(platforms: .some([.iOS])))
             ],
             path: "iProovTargets"
         )
